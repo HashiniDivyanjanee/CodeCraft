df529ee2e29ce86a2ed0dc72e6484e640b3491cf
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2025-04-24 20:21:17 +0530

    Refactor Hero component: enhance layout and add content

--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import heroImg from "../assets/2.png";
 import { div } from "framer-motion/client";
+import { IoArrowForwardCircleSharp } from "react-icons/io5";
 const Hero = () => {
   return (
     <section
@@ -10,8 +11,9 @@ const Hero = () => {
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between p-8 overflow-hidden gap-12 h-full">
         {/* left side */}
         <div className="md:w-1/2">
-          <h1>Code Craft Innovations</h1>
-          <p></p>
+          <h1 className="text-4xl font-secondary font-bold mb-4 md:w-3/5 leading-snug">CodeCraft Innovations</h1>
+          <p className="text-lg m-12 md:pr-8">Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.</p>
+        <button><a href="#contact"><span>Get Started</span> <IoArrowForwardCircleSharp /></a></button>
         </div>
 
         {/* right side */}
