1814e6d51ff8b15e2b268ecfd9a2be7abc5d6527
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2025-04-25 22:19:51 +0530

    Add Services component with tabs and update dependencies
    - Implemented a new Services component featuring tabbed navigation for different service categories.
    - Added 'react-tabs' dependency for tab functionality.
    - Updated styles for the tabs in App.css.
    - Included a background image asset for the component.

--- src/component/services.jsx
+++ src/component/services.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+
 
 const Services = () => {
   return (
-    <div id='services' className='h-screen bg-amber-200'>Services</div>
-  )
-}
+    <div id="services" className="h-screen bg-white">
+      <div className="pt-28 px-4 container mx-auto">
+        <div className="text-center space-y-5">
+          <h2 className="text-4xl font-bold text-heroBg">
+            What can we do Together
+          </h2>
+          <p className="md:w-1/2 mx-auto">
+            {" "}
+            Video provides a powerful way to help you prove your point. When you
+            click Online Video, you can paste in the embed code for the video
+            you want to add. You can also type a keyword to search online for
+            the video that best fits your document.
+          </p>
+        </div>
 
-export default Services
+        {/* Service Category Section */}
+        <div className="py-12 md:w-4/5 mx-auto">
+          <Tabs>
+            <TabList className="flex flex-wrap justify-between items-center md:gap-8 gap-4">
+              <Tab>Web Development</Tab>
+              <Tab>Mobile Development</Tab>
+              <Tab>Custom Software</Tab>
+              <Tab>Digital Marketing</Tab>
+            </TabList>
+
+            <TabPanel>
+              <h2>Any content 1</h2>
+            </TabPanel>
+            <TabPanel>
+              <h2>Any content 2</h2>
+            </TabPanel>
+            <TabPanel>
+              <h2>Any content 2</h2>
+            </TabPanel>
+            <TabPanel>
+              <h2>Any content 2</h2>
+            </TabPanel>
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
